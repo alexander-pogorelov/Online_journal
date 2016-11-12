e73ca434abdeabe7d74293ad0511563441e13f16
Погорелов Александр Анатольевич <ics.price@gmail.com>
2016-11-12 17:08:29 +0300

    add entity and mapping

---
+++ src/Application/Sonata/UserBundle/Entity/Journal.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex_PL
+ * Date: 12.11.2016
+ * Time: 13:49
+ */
+
+namespace Application\Sonata\UserBundle\Entity;
+
+
+class Journal
+{
+    private $id;
+
+    private $lesson;
+
+    private $pupilGroup;
+
+    private $isAbsent; // если 1, значит отсутствует на уроке
+
+    private $assessment; // оценка по предмету
+
+    private $remark; // комментарий к оценке
+
+
+    /**
+     * Set isAbsent
+     *
+     * @param boolean $isAbsent
+     *
+     * @return Journal
+     */
+    public function setIsAbsent($isAbsent)
+    {
+        $this->isAbsent = $isAbsent;
+
+        return $this;
+    }
+
+    /**
+     * Get isAbsent
+     *
+     * @return boolean
+     */
+    public function getIsAbsent()
+    {
+        return $this->isAbsent;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set remark
+     *
+     * @param string $remark
+     *
+     * @return Journal
+     */
+    public function setRemark($remark)
+    {
+        $this->remark = $remark;
+
+        return $this;
+    }
+
+    /**
+     * Get remark
+     *
+     * @return string
+     */
+    public function getRemark()
+    {
+        return $this->remark;
+    }
+
+    /**
+     * Set assessment
+     *
+     * @param integer $assessment
+     *
+     * @return Journal
+     */
+    public function setAssessment($assessment)
+    {
+        $this->assessment = $assessment;
+
+        return $this;
+    }
+
+    /**
+     * Get assessment
+     *
+     * @return integer
+     */
+    public function getAssessment()
+    {
+        return $this->assessment;
+    }
+
+    /**
+     * Set pupilGroup
+     *
+     * @param \Application\Sonata\UserBundle\Entity\PupilGroupAssociation $pupilGroup
+     *
+     * @return Journal
+     */
+    public function setPupilGroup(\Application\Sonata\UserBundle\Entity\PupilGroupAssociation $pupilGroup = null)
+    {
+        $this->pupilGroup = $pupilGroup;
+
+        return $this;
+    }
+
+    /**
+     * Get pupilGroup
+     *
+     * @return \Application\Sonata\UserBundle\Entity\PupilGroupAssociation
+     */
+    public function getPupilGroup()
+    {
+        return $this->pupilGroup;
+    }
+}
