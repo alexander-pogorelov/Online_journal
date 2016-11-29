495dd22c87260c12ebc2a200b3728e2d5d30a44d
Погорелов Александр Анатольевич <ics.price@gmail.com>
2016-11-29 23:00:55 +0300

    refactoring code 2

--- src/AppBundle/Controller/CRUDController.php
+++ src/AppBundle/Controller/CRUDController.php
@@ -14,45 +14,7 @@ class CRUDController extends Controller
 {
 
 
-    public function showPupilsInGroupAction($id = null)
-    {
-        $request = $this->getRequest();
-        $id = $request->get($this->admin->getIdParameter());
 
-        $object = $this->admin->getObject($id);
-
-        if (!$object) {
-            throw $this->createNotFoundException(sprintf('unable to find the object with id : %s', $id));
-        }
-
-        $repository = $this->getDoctrine()->getRepository('ApplicationSonataUserBundle:PupilGroupAssociation');
-
-        $pupilGroupAssociations = $repository->findBy([
-            'group' => $id
-        ]);
-
-        $pupilsObjects = [];
-        foreach ($pupilGroupAssociations as $pupilGroupAssociation) {
-            $pupilsObjects[] = $pupilGroupAssociation->getPupil();
-        }
-
-        $this->admin->checkAccess('show', $object);
-
-        $preResponse = $this->preShow($request, $object);
-        if ($preResponse !== null) {
-            return $preResponse;
-        }
-
-        $this->admin->setSubject($object);
-
-
-        return $this->render('AppBundle:GroupAdmin:pupils_show.html.twig', [
-            'action'=>'showPupilsInGroup',
-            'object' => $object,
-            'pupilsObjects' => $pupilsObjects,
-            'elements' => $this->admin->getShow(),
-        ]);
-    }
 
     public function showAction($id = null)
     {
